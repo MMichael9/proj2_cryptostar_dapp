a1387ae02809bcb3caacdc0cd259c6a0af513841
Michael Minhas <michael.minhas@hotmail.com>
2021-04-19 18:12:47 -0700

    add truffe config

--- truffle-config.js
+++ truffle-config.js
@@ -28,9 +28,9 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "<Infura PROJECT ID>";
+const infuraKey = "e982b8dc204e4b2491e4f2ad28264673";
 
-const mnemonic = "<METAMASK SEED>";
+const mnemonic = "depart library surge dog false behave fair accident hinge door vehicle micro";
 
 module.exports = {
   /**
@@ -54,6 +54,12 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
     },
 
     // Another network with more advanced options...
